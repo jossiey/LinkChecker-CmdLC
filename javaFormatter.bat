052df8f467287a653ed1752619b29ea546de37f4
jossiey <jyang223@myseneca.ca>
2020-11-12 00:50:32 -0500

    add google java format tool  add SpotBugs tool  add CONTRIBUTING.md file  update CmdLC.jar file  add plug-in and git pre-commit set up
    remove javaFormatter.bat file, duplicated

--- javaFormatter.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-:: Section : Google Java Format
-ECHO ============================
-ECHO FORMATTER CHECKING
-ECHO ============================
-java -jar lib/google-java-format-1.9-all-deps.jar --replace src/osd/*.java
-ECHO Done
-PAUSE
